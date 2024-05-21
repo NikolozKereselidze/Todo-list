90f359094da6a048e3ff60aa0670037a5c810c6f
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-05-21 13:25:13 +0400

    feat: install file loader and configure it

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         include: path.resolve(__dirname, "./src/css"),
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader",
+      },
     ],
   },
   output: {
