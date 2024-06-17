e7a947cf22847ed559856af244630346216533d0
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-06-17 19:20:57 +0400

    feat: display tasks individually

--- src/modules/displayTask.js
+++ src/modules/displayTask.js
@@ -1,5 +1,19 @@
+import { createIcon } from "./initial";
+
 const taskContainer = document.createElement("div");
 taskContainer.classList.add("task-container");
+
+function checkPriority(priority) {
+  let icon;
+  if (priority === "low") {
+    icon = createIcon("fa-gauge-low");
+  } else if (priority === "medium") {
+    icon = createIcon("fa-gauge");
+  } else if (priority === "high") {
+    icon = createIcon("fa-gauge-high");
+  } else return;
+  return icon;
+}
 
 export function displayTask() {
   const mainDiv = document.querySelector(".task-section");
@@ -12,22 +26,29 @@ export function displayTask() {
     const activeProject = projects[activeProjectSpan.textContent];
     activeProject.forEach((el) => {
       const taskWrapper = document.createElement("div");
-      const taskCaller = document.createElement("p");
+      const taskCaller = document.createElement("span");
       taskCaller.textContent = activeProjectSpan.textContent;
+      const userIcon = createIcon("fa-user");
       const taskDiv = document.createElement("div");
       taskDiv.classList.add("task-information");
       const taskTitle = document.createElement("h3");
       taskTitle.textContent = el.title;
       const taskDate = document.createElement("time");
       taskDate.textContent = el.date;
+      const dateIcon = createIcon("fa-calendar");
       const taskDescription = document.createElement("p");
       taskDescription.textContent = el.description;
       const taskPriority = document.createElement("p");
       taskPriority.textContent = el.priority;
 
-      taskWrapper.appendChild(taskDate);
-      taskWrapper.appendChild(taskCaller);
-      taskWrapper.appendChild(taskPriority);
+      const priorityIcon = checkPriority(el.priority);
+
+      dateIcon.appendChild(taskDate);
+      userIcon.appendChild(taskCaller);
+      taskWrapper.appendChild(dateIcon);
+      //   taskWrapper.appendChild(taskDate);
+      taskWrapper.appendChild(userIcon);
+      taskWrapper.appendChild(priorityIcon);
       taskDiv.appendChild(taskTitle);
       taskDiv.appendChild(taskDescription);
       taskDiv.appendChild(taskWrapper);
